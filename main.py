9cffd453903db8af539073631f70b5497b5254e1
Alexandre Bonnet <alexandre.bonnet@edu.esiee.fr>
2026-06-22 10:51:32 +0200

    added render template in main

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-def main():
-    return 0
+import mysql.connector
+from flask import Flask, render_template,request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/connexion",methods=["GET","POST"])
+def connexion():
+    return render_template("appli.html")
+  
